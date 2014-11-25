4a0450df135ad16aed8a956c3c673a38fd060ab0
satrianachandra <satrianachandra@gmail.com>
2014-11-25 15:45:48 +0100

    add audio mute

--- src/senderreceiverpipe/SenderPipeline.java
+++ src/senderreceiverpipe/SenderPipeline.java
@@ -34,8 +34,8 @@ public class SenderPipeline extends Pipeline{
     private BaseSrc src = (BaseSrc) ElementFactory.make("alsasrc", null);
     
     //from v4l2 (webcam)
-    //private BaseSrc srcV = (BaseSrc) ElementFactory.make("v4l2src", null);
-    private BaseSrc srcV = (BaseSrc) ElementFactory.make("videotestsrc", null);
+    private BaseSrc srcV = (BaseSrc) ElementFactory.make("v4l2src", null);
+    //private BaseSrc srcV = (BaseSrc) ElementFactory.make("videotestsrc", null);
     
     private final Element tee = ElementFactory.make("tee", "teeA");
     // THE SenderBin to talk with somebody
@@ -65,7 +65,7 @@ public class SenderPipeline extends Pipeline{
         
         //Video
         //srcV.set("device", "/dev/video0");
-        srcV.set("pattern", 1);
+        //srcV.set("pattern", 1);
         srcV.setLive(true);
         addMany(srcV,teeV);
         Util.doOrDie("src-tee", linkMany(srcV, teeV));
